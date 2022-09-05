8f0749764676450a728a4bb9e63bec533588a755
syrizaldev <syahrizaldev@gmail.com>
2022-09-06 01:17:31 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Responsive Megamenu
-
-- Create using HTML5, SCSS, and Vanilla JavaScript.
-- Developed with Desktop First Methodology.
-- Responsive and Compatible with all Devices Screen.
+# Vanilla Megamenu
--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,12 @@ const menuNavbar = document.querySelector('.navbar');
 const menuInner = menuNavbar.querySelector('.menu-inner');
 const menuArrow = menuNavbar.querySelector('.menu-arrow');
 const menuBurger = document.querySelector('.burger');
-const overlay = document.querySelector('.overlay');
+const overlayBg = document.querySelector('.overlay');
 
-// Show Hide Navbar Menu Function
+// Navbar Menu Toggle Function
 const toggleMenu = () => {
 	menuNavbar.classList.toggle('active');
-	overlay.classList.toggle('active');
+	overlayBg.classList.toggle('active');
 };
 
 // Hide Mobile Submenu Function
@@ -42,18 +42,33 @@ const toggleSubMenu = (e) => {
 	}
 };
 
-// Fixed Resize Window Function
-const resizeWindow = () => {
-	if (window.innerWidth > 991) {
+// Load Image use Intersection Observer
+if ('IntersectionObserver' in window) {
+	const intersection = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.src = entry.target.dataset.src;
+				observer.unobserve(entry.target);
+			}
+		});
+	});
+
+	document.querySelectorAll('img').forEach((image) => {
+		intersection.observe(image);
+	});
+}
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener('resize', () => {
+	if (window.innerWidth >= 992) {
 		if (menuNavbar.classList.contains('active')) {
 			toggleMenu();
 		}
 	}
-};
+});
 
-// Initialize Event Listeners
+// Initialize All Event Listeners
 menuBurger.addEventListener('click', toggleMenu);
-overlay.addEventListener('click', toggleMenu);
+overlayBg.addEventListener('click', toggleMenu);
 menuArrow.addEventListener('click', hideSubMenu);
 menuInner.addEventListener('click', toggleSubMenu);
-window.addEventListener('resize', resizeWindow);
